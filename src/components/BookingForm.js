659f185b14e66e70178e45416afec9eb29554ca6
malebowsky <mattlebowsky87@gmail.com>
2023-04-13 12:26:21 +0200

    accessibility tags

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 
 const BookingForm = ({
     formValue,
@@ -7,36 +6,31 @@ const BookingForm = ({
     setAvailiableTimes,
     submitForm
 }) => {
-    
-    useEffect(() => {
-        console.log(formValue);
-    }, [formValue]);
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
             <form style={{ display: 'grid', maxWidth: '200px', gap: '16px' }} onSubmit={submitForm}>
                 <label htmlFor="res-date">Choose date</label>
-                <input type="date" id="res-date" name="res-date" onChange={(e) => {
+                <input type="date" required id="res-date" name="res-date" aria-label="Select date" onChange={(e) => {
                     setFormValue({ ...formValue, date: e.target.value });
-                    console.log(e.target.value)
                     setAvailiableTimes(e.target.value);
                 }} />
                 <label htmlFor="res-time">Choose time</label>
-                <select id="res-time" onChange={(e) => setFormValue({ ...formValue, time: e.target.value })}>
+                <select id="res-time" aria-label="Choose available time" required onChange={(e) => setFormValue({ ...formValue, time: e.target.value })}>
                     <option value="">Choose an option</option>
                     {!!availiableTimes && availiableTimes.map((time, index) => (
                         <option key={index}>{time}</option>
                     ))}
                 </select>
                 <label htmlFor="guests">Number of guests</label>
-                <input type="number" id="guests" name="guests" min="1" max="10" placeholder="1" onChange={(e) => setFormValue({ ...formValue, guest: e.target.value })} />
+                <input type="number" id="guests" name="guests" aria-label="Number of guests" required min="1" max="10" placeholder="1" onChange={(e) => setFormValue({ ...formValue, guest: e.target.value })} />
                 <label htmlFor="ocassion">Ocassion</label>
-                <select id="ocassion" onChange={(e) => setFormValue({ ...formValue, ocassion: e.target.value })}>
+                <select id="ocassion" aria-label="Choose ocassion" required onChange={(e) => setFormValue({ ...formValue, ocassion: e.target.value })}>
                     <option value="">Choose an option</option>
                     <option>Anniversary</option>
                     <option>Birthday</option>
                 </select>
-                <button type="submit">
+                <button type="submit" aria-label="On Click">
                     Make your reservation
                 </button>
             </form>
